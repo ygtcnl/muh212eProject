1ef69360541160c14f8410b3243f93377827bac8
Yiğitcan Karaaslan <ygtcnl_karaaslan@hotmail.com>
2020-11-28 21:16:56 +0300

    detailed explanation added

--- pancakeSortFromUserInput.cpp
+++ pancakeSortFromUserInput.cpp
@@ -5,10 +5,10 @@ using namespace std;
 //int findMax(int arr[],int endpoint) fonksiyonu verilen arrayde 0.indexinden endpoint-1 indexine kadar olan maksimum integer değerinin bulunduğu indexi dönüyor
 int findIndexOfMax(int arr[],int endpoint)  
 {
-    int index=0;
-    for(int i=0;i<endpoint;i++)
+    int index=0; //en başta max olan index 0 olarak atanıyor
+    for(int i=0;i<endpoint;i++) //arrayin elemanları üstünde geziniyoruz
     {
-        if(arr[index]<arr[i])
+        if(arr[index]<arr[i]) //eğer i indexindeki değer daha büyükse indexi i yapıyoruz
             index=i;
     }
     return index;
@@ -17,10 +17,10 @@ int findIndexOfMax(int arr[],int endpoint)
 //void flip(int arr[],int endpoint) fonksiyonu verilen arrayde 0 indexiyle endpoint indexine kadar olan subarrayi ters çeviriyor
 void flip(int arr[],int endpoint)
 {
-    int curr=0;
-    while(curr<endpoint)
+    int curr=0; //curr şuan bulunduğumuz indexi belirtiyor
+    while(curr<endpoint) //two pointer şeklinde ilerlediğimizden pointerların karşılaşıp birbirlerini geçmemesini kontrol ediyoruz
     {
-        int temp=arr[curr];
+        int temp=arr[curr]; //ek bir integer oluşturuyoruz arr[curr] ile arr[endpoint] in yerlerini değiştirirken işimize yarıyor
         arr[curr]=arr[endpoint];
         arr[endpoint]=temp;
         curr++;
@@ -33,9 +33,9 @@ void pancakeSort(int arr[],int size)
 {
     for(int curr=size;curr>1;curr--)
     {
-        int maxIndex=findIndexOfMax(arr,curr);
-        flip(arr,maxIndex);
-        flip(arr,curr-1);
+        int maxIndex=findIndexOfMax(arr,curr); //"curr" e kadar olan maksimum değerin indexini buluyoruz
+        flip(arr,maxIndex); //çeviriyoruz
+        flip(arr,curr-1); //bir daha çeviriyoruz
     }
 }
 
